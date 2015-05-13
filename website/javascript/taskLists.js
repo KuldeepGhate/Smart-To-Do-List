51159de16e6262e0a0f2ed4435984d1fccc750d9
Joseph Pariseau <jpariseau@stellatechnology.com>
2015-05-13 16:31:12 -0700

    fixed the broken refactor

--- website/javascript/taskLists.js
+++ website/javascript/taskLists.js
@@ -93,7 +93,7 @@ function TasksLists() {
     };
 }
 
-function tasksAndTagsTest() {
+function TasksListsTest() {
     var tag1 = new Tag("school", "red");
     var tag2 = new Tag("work", "green");
     var tag3 = new Tag("life", "blue");
@@ -102,7 +102,7 @@ function tasksAndTagsTest() {
     var task2 = new Task("homework", "homework sucks", "important", [tag3, tag2], 201505312121);
     var task3 = new Task("fun", "fun is good", "default", [tag1, tag3], 201505061213);
 
-    var masterList = new TasksAndTags();
+    var masterList = new TasksLists();
 
     masterList.addTag(tag1);
     masterList.addTag(tag2);
