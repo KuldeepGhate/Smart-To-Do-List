08a89fe780d1d80f0c88fcfe9a0cefe26bae569d
Kuldeep Ghate <kuldeepghate@gmail.com>
2015-05-15 20:06:22 -0700

    tag and tasks classes

--- website/javascript/tableScripts.js
+++ website/javascript/tableScripts.js
@@ -1,19 +1,78 @@
+var taskNumber = 0;
 $(document).ready(function () {
-    $("#hello").dialog({autoOpen: false,
+    $("#form").dialog({autoOpen: false,
         maxHeight: 600,
         maxWidth: 600,
         height: 600,
         width: 600
     });
     $(".add").click(function () {
-        $("#hello").dialog("open");
+        $("#form").dialog("open");
+        $("#form").empty();
+        $("div#form").append(
+
+            $("<h2/>").text("Task Manager"), 
+
+            //Task Name
+            $("<label>Task Name: </label>"),
+            $("<input/>", { type: 'text',id: 'task'+taskNumber, name: 'name', placeholder: 'Your Name'}), 
+            $("<br/>"),$("<br/>"),
+
+            // Due Date
+            $("<label>Due Date: </label>"),
+            $("<input/>", {type: 'text', id: 'datepicker',placeholder: 'Select Date'}), 
+            $("<br/>"),$("<br/>"),
+
+            //Time
+            $("<label>Time: </label>"), 
+            $("<input/>", {type: 'time', id: 'taskTime'}),
+            $("<br/>"),$("<br/>"),
+
+            //Alarm
+            $("<label>Alarm: </label>"),
+            $("<input/>", {type: 'radio', name: 'yes', value: 'yes'}),
+            $("<label>Yes </label>"), 
+            $("<input/>", {type: 'radio', name: 'no', value: 'no'}),
+            $("<label>No </label>"),
+            $("<br/>"),$("<br/>"),
+
+            //How long before
+            $("<label>How long before? </label>"),
+            $("<input/>", {type: 'number', name: 'time', min:'1', max: '60'}),
+            $("<select><option value='hours'>Hours</option><option value='minutes'>Minutes</option></select>"),
+            $("<br/>"),$("<br/>"),
+
+            //Tags
+            $("<label>Tags </label>"),
+            $("<input name:'tag', type:'text'>").attr({"placeholder": 'Tag',"id":'tag'+taskNumber}),
+            $("<br/>"),$("<br/>"),
+
+            //Tag Color
+            $("<label>Priority </label>"),
+            $("<input id:'tagColor' name:'tagColor' type:'text'>").attr({"placeholder": 'Color',"id":'color'+taskNumber}),
+            $("<br/>"),$("<br/>"),
+
+            //Button
+            $("<button class:'add' type:'button' onclick:exec() >Add</button>").click(
+                function(){
+                    var table = document.getElementById("myTable");
+                    var row = table.insertRow(-1);
+                    var cell1 = row.insertCell(-1);
+                    var cell2 = row.insertCell(-1);
+                    cell1.innerHTML = $("#task"+taskNumber).val();
+                    cell2.innerHTML = $("#tag"+taskNumber).val();
+                    $("#form").dialog("close");
+                    taskNumber++;
+                }
+            )//click
+        )//append
+
     });
+    $(function() {
+            $( "#datepicker" ).datepicker();
+    }); 
 });
 
 function exec() {
-    $("#hello").dialog("close");
-    var table = document.getElementById("myTable");
-    var row = table.insertRow(-1);
-    var cell1 = row.insertCell(-1);
-    cell1.innerHTML = $("#task").val();
+    
 }
