8d82df00d942c9c0c51e59bd5f0a48cc85d7c01a
Joseph Pariseau <jpariseau@stellatechnology.com>
2015-06-01 14:02:21 -0700

    merging the rest of the php html files

---
+++ website/html/form.php
@@ -0,0 +1,116 @@
+<!DOCTYPE html>
+<html>
+<head lang="en">
+    <meta charset="UTF-8">
+    <link rel="stylesheet" type="text/css" href="../style/style.css">
+    <link rel="stylesheet" href="../style/jquery-ui.css">
+    <title>Todo List</title>
+    <script src="../javascript/jQuery/jquery-2.1.3.js"></script>
+    <script src="../javascript/jQuery/jquery-ui.js"></script>
+    <script src="../javascript/tag.js"></script>
+    <script src="../javascript/task.js"></script>
+    <script src="../javascript/taskLists.js"></script>
+    <script src="../javascript/initialize.js"></script>
+    <script src="../javascript/events.js"></script>
+
+</head>
+
+<body class="wallpaper">
+<div id="container">
+    <div id="header">
+        <h1 class="big-heading">The Best to do List</h1>
+    </div>
+
+    <!-- The Lists -->
+    <div id="content">
+        <h2 class="medium-heading">List items</h2>
+
+        <div id="todo">
+            <!-- To do list -->
+            <div id="divTasksTable">
+                <!-- cellspacing='0' is important, must stay -->
+                <table id="todoTasks" cellspacing='0'>
+                </table>
+            </div>
+
+            <form id="addTaskForm1" method="post">
+                <h2>Task Manager</h2>
+                <label>Task Name: </label><input type='text' id='addTaskName' name='taskName'><br><br>
+                <label>Task Description: </label><input type='text' id='addTaskDescription'
+                                                        name='taskDescription'><br><br>
+                <label>Due Date:</label><input type='datetime-local' id='addDate' placeholder='Select Date'
+                                               name='dueDate'><br><br>
+                <label>Alarm: </label><input type='checkbox' id='addAlarm' name='yes' value='true'><br><br>
+                <label>How long before would you like the alarm? </label><input type='number' id='addAlarmDate'
+                                                                                name='time' min='1' max='60'>
+                <select id='timeSelect'>
+                    <option value='hours'>Hours</option>
+                    <option value='minutes'>Minutes</option>
+                </select><br><br>
+                <label>Tags </label><input name='tag' id='addTagName' type='text' placeholder='Tag'><br><br>
+                <label>Priority </label><input id='addTagColor' name='tagColor' type='text' placeholder='Color'><br><br>
+                <input id='addTask' type='submit' value='Add' name='submit'>
+            </form>
+            <!-- To do list -->
+            <div id="divFinishedTable">
+                <!-- cellspacing='0' is important, must stay -->
+                <table id="finishedTasks" cellspacing='0'>
+                </table>
+            </div>
+            <a href="#" id="addTask" class="button">Add item</a>
+            <a href="#" id="removeTask" class="button">Remove item</a>
+        </div>
+    </div>
+
+    <!-- Popup Div -->
+
+    <div id="footer">
+        <h5 class="small-heading"><a href="https://github.com/Bluesroo/COEN-276-Project">GitHub</a></h5>
+    </div>
+</div>
+<?php
+
+
+$taskName = $_POST["taskName"];
+$taskDescription = $_POST["taskDescription"];
+$xmlFile = simplexml_load_file("task.xml");
+//  $dueDate = $_POST["dueDate"];
+$taskTag = $_POST["tag"];
+$taskColor = $_POST["tagColor"];
+
+$doc = new DOMDocument();
+$doc->load('task.xml');
+
+//grab a node
+$xpath = new DOMXPath($doc);
+$results = $xpath->query('/tasklist');
+$new_task = $results->item(0);
+
+//create a new, free standing node
+$task = $doc->createElement('task');
+//task description
+$description = $doc->createElement("description");
+$description->appendChild($doc->createTextNode($taskDescription));
+$task->appendChild($description);
+
+//task tag
+$tag = $doc->createElement("tag");
+$tag->appendChild($doc->createTextNode($taskTag));
+$task->appendChild($tag);
+
+//task Color
+$color = $doc->createElement("color");
+$color->appendChild($doc->createTextNode($taskColor));
+$task->appendChild($color);
+
+//append our new node to the node we pulled out
+$new_task->appendChild($task);
+
+//output original document.  $baz_nod_of_doc is
+
+$doc->save("task.xml")
+
+?>
+
+</body>
+</html>
