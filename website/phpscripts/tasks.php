8809cd501469900bcfd6e0217807e97dd03b8fe4
Joseph Pariseau <jpariseau@stellatechnology.com>
2015-06-02 20:04:31 -0700

    file cleanup

--- website/phpscripts/tasks.php
+++
@@ -1,8 +0,0 @@
-<?php
-$taskName = $_POST["taskName"];
-$taskDescription = $_POST["taskDescription"];
-$dueDate = $_POST["dueDate"];
-$tag = $_POST["tag"];
-$tagColor = $_POST["tagColor"];
-echo "string";
-?>
