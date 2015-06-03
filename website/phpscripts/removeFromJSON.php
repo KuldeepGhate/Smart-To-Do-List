5321c85adcb9e4c10d1a647b7409f79cad534e86
Kuldeep Ghate <kuldeepghate@gmail.com>
2015-06-03 01:22:36 -0700

    json

--- website/phpscripts/removeFromJSON.php
+++ website/phpscripts/removeFromJSON.php
@@ -2,14 +2,17 @@
 function removeFromJSON($id) {
     $jsonArray = file_get_contents('../data/taskList.json', true);
     $decodedArray = json_decode($jsonArray);
-    foreach ($decodedArray as $key => $entry) {
+     foreach ($decodedArray as $key => $entry) {
         foreach ($entry as $value) {
+            # code..1.
             if ($value->id == $id) {
-                unset($decodedArray->$key[$value]);
+                $value->taskName="taskName";
+
+            
             }
         }
     }
     $newJsonArray = json_encode($decodedArray);
     file_put_contents('../data/taskList.json', $newJsonArray);
-    return 100;
+    return $newJsonArray;
 }
