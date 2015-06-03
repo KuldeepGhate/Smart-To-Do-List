b2b85b75ceff6933eb9ef48d67a452602a07da2a
Joseph Pariseau <jpariseau@stellatechnology.com>
2015-06-02 18:41:33 -0700

    php updates

--- website/phpscripts/tasks.php
+++ website/phpscripts/tasks.php
@@ -1,18 +1,8 @@
-<!DOCTYPE html>
-<html>
-<body>
-<script>
-	alert("alert!!");
-</script>
 <?php
-//include 'ChromePhp.php';
-	$taskName = $_POST["taskName"];
-	$taskDescription = $_POST["taskDescription"];
-	$dueDate = $_POST["dueDate"];
-	$tag = $_POST["tag"];
-	$tagColor = $_POST["tagColor"];
-	echo "string";
-//	ChromePhp::log('Hello console!');
+$taskName = $_POST["taskName"];
+$taskDescription = $_POST["taskDescription"];
+$dueDate = $_POST["dueDate"];
+$tag = $_POST["tag"];
+$tagColor = $_POST["tagColor"];
+echo "string";
 ?>
-</body>
-</html>
